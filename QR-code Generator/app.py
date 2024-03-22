92f078d41b18cf81750375f47f8091a3db4cddbd
George Gök <george_geneta@hotmail.com>
2024-03-22 15:02:23 +0100

    changes

--- QR-code Generator/app.py
+++ QR-code Generator/app.py
@@ -76,7 +76,51 @@ def generate_qr_codes(quantity, sku, folder_date):
 # Hemrutt för att rendera indexsidan
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
     return render_template('form.html')
+
+@app.route('/button')
+def button():
+    return render_template('button.html')
+
+@app.route('/chart')
+def chart():
+    return render_template('chart.html')
+
+@app.route('/element')
+def element():
+    return render_template('element.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
+@app.route('/typography')
+def typography():
+    return render_template('typography.html')
+
+@app.route('/widget')
+def widget():
+    return render_template('widget.html')
+
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+@app.route('/fyranollfyra')
+def fyranollfyra():
+    return render_template('404.html')
 
 # Rutten för att generera QR-koder baserat på formulärdata
 @app.route('/generate_qr', methods=['POST'])
